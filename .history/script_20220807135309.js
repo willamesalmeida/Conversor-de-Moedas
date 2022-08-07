53080dd1f7d456243248b3d4e87c926e510a37c8
José Willames <willames5480@gmail.com>
2022-08-07 16:11:04 -0300

    Conversor de Moedas
    Conversor de Moedas

---
+++ .history/script_20220807135309.js
@@ -0,0 +1,49 @@
+function Converter() {
+  let input = parseFloat(document.getElementById("valor_inicial").value);
+  let output = document.getElementById("resultado");
+  let de = document.getElementById("moedasde");
+  let para = document.getElementById("moedaspara");
+
+  console.log(de.value);
+
+  //Reais
+  if (de.value == "BRL" && para.value != "Moeda 2") {
+    let valorEmReais = input;
+    let valor;
+    if (para.value == "BRL") {
+      valor = "O valor em reais é: R$" + valorEmReais.toFixed(2);
+    } else if (para.value == "EUR") {
+      valor = "O valor em euros é: €" + (valorEmReais * 0.16).toFixed(2);
+    } else if (para.value == "USD") {
+      valor = "O valor em dólar é: $" + (valorEmReais * 0.19).toFixed(2);
+    }
+    output.innerHTML = valor;
+  } else if (de.value == "EUR" && para.value != "Moeda 2") {
+    let valorEmEuros = input;
+    let valor;
+
+    if (para.value == "EUR") {
+      valor = "O valor em euros é: €" + valorEmEuros.toFixed(2);
+    } else if (para.value == "USD") {
+      valor = "O valor em dólar é: $" + (valorEmEuros * 1.0167).toFixed(2);
+    } else if (para.value == "BRL") {
+      valor = "O valor em reais é: R$" + (valorEmEuros * 5.303).toFixed(2);
+    }
+    output.innerHTML = valor;
+  } else if (de.value == "USD" && para.value != "Moeda 2") {
+    console.log("entrou no else is do dolar");
+    let valorEmDolar = input;
+    let valor;
+
+    if (para.value == "USD") {
+      valor = "O valor em dólar é: $" + valorEmDolar.toFixed(2);
+    } else if (para.value == "BRL") {
+      valor = "O valor em dólar é: R$" + (valorEmDolar * 5.2159).toFixed(2);
+    } else if (para.value == "EUR") {
+      valor = "O valor em dólar é: €" + (valorEmDolar * 0.9832).toFixed(2);
+    }
+    output.innerHTML = valor;
+  } else {
+    output.innerHTML = "";
+  }
+}
